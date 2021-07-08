33cce010f1297ce4686ed9cc160f57675018a312
Hasan Haider <31242825+HasanHaider1@users.noreply.github.com>
2021-07-08 15:04:11 +0500

    Video wala myProducts

--- MyProducts/src/Resources/app/administration/src/main.js
+++ MyProducts/src/Resources/app/administration/src/main.js
@@ -1,1 +1,3 @@
 import './module/sw-cms/elements/dailymotion';
+import './module/sw-cms/blocks/video/dailymotion-video';
+
---
+++ MyProducts/src/Resources/app/administration/src/module/sw-cms/blocks/video/dailymotion-video/preview/index.js
@@ -0,0 +1,6 @@
+import template from './sw-cms-preview-dailymotion-video.html.twig';
+import './sw-cms-preview-dailymotion-video.scss';
+const { Component } = Shopware;
+Shopware.Component.register('sw-cms-preview-dailymotion-video', {
+    template
+});
