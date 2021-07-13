9dad8518a8b807b898151fda1d6f0372a671603e
Hasan Haider <31242825+HasanHaider1@users.noreply.github.com>
2021-07-13 17:43:46 +0500

    update

---
+++ MyAboshop/src/Subscriber/MySubscriber.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace MyAboshop\Subscriber;
+
+use Shopware\Storefront\Page\GenericPageLoadedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Shopware\Core\Framework\Struct\ArrayEntity;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
+
+class MySubscriber implements EventSubscriberInterface
+{
+    protected $SystemConfig;
+    public function __construct(SystemConfigService $SystemConfig)
+    {
+        $this->SystemConfig = $SystemConfig;
+    }
+
+
+    public static function getSubscribedEvents(): array
+    {
+        // Return the events to listen to as array like this:  <event to listen to> => <method to execute>
+        return [
+            GenericPageLoadedEvent::class => 'onPageLoaded'
+        ];
+    }
+
+    public function onPageLoaded(GenericPageLoadedEvent $event)
+    {
+        // Do something
+        // E.g. work with the loaded entities: $event->getEntities()
+        $something = $this->SystemConfig->get('Hello');
+        $configMinDate = $this->SystemConfig->get('MyAboshop.config.mindate');
+        $configMaxDate = $this->SystemConfig->get('MyAboshop.config.maxdate');
+        $configDisabledDate = $this->SystemConfig->get('MyAboshop.config.disableddate');
+
+        $array = ['key1' => $configMinDate,'key2' => $configMaxDate,'key3' => $configDisabledDate];
+        //assign the array to the page
+        $event->getPage()->assign($array);
+
+        //add the array to the page as an extension
+        $event->getPage()->addExtension('testPageExtension', new ArrayEntity($array));
+
+        //assign the array to the context
+        $event->getContext()->assign($array);
+
+        //add the array to the context as an extension
+        $event->getContext()->addExtension('testContextExtension', new ArrayEntity($array));
+
+    }
+}
