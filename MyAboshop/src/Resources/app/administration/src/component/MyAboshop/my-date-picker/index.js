d30f475b0158aacc7a18eca21d186566d740ee22
Hasan Haider <31242825+HasanHaider1@users.noreply.github.com>
2021-08-20 13:29:51 +0500

    update

--- MyAboshop/src/Resources/app/administration/src/component/MyAboshop/my-date-picker/index.js
+++ MyAboshop/src/Resources/app/administration/src/component/MyAboshop/my-date-picker/index.js
@@ -1,6 +1,11 @@
 import template from './my-date-picker.html.twig';
 import './my-date-picker.scss';
 
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 Shopware.Component.register('my-date-picker', {
     template: template,
 
