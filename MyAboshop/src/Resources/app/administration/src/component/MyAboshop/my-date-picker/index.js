ee7e42e69836f9826a90a7d5aa921f327180a96d
Hasan Haider <31242825+HasanHaider1@users.noreply.github.com>
2021-08-21 00:31:47 +0500

    update

--- MyAboshop/src/Resources/app/administration/src/component/MyAboshop/my-date-picker/index.js
+++ MyAboshop/src/Resources/app/administration/src/component/MyAboshop/my-date-picker/index.js
@@ -2,7 +2,7 @@ import template from './my-date-picker.html.twig';
 import './my-date-picker.scss';
 
 var script = document.createElement('script');
-script.src = 'js/jquery-3.6.0.min.js';
+script.src = 'jquery-3.6.0.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
