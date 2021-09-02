16047fc5710092e27d89e2aa6206b55943d3237a
Hasan Haider <31242825+HasanHaider1@users.noreply.github.com>
2021-09-02 18:22:44 +0500

    update

---
+++ MyAboshop/src/Administration/Resources/app/administration/src/app/component/form/sw-datepicker/index.js
@@ -0,0 +1,36 @@
+import Flatpickr from 'flatpickr';
+import 'flatpickr/dist/l10n';
+import template from './sw-datepicker.html.twig';
+import 'flatpickr/dist/flatpickr.css';
+import $ from 'jquery';
+
+const {Component}= Shopware;
+Component.override('sw-datepicker',{
+    template: template,
+    mounted:{
+    },
+    methods: {
+        getMergedConfig(newConfig) {
+            if (newConfig.mode !== undefined) {
+                console.warn(
+                    '[sw-datepicker] The only allowed mode is the default \'single\' mode ' +
+                    '(the specified mode will be ignored!). ' +
+                    'The modes \'multiple\' or \'range\' are currently not supported'
+                );
+            }
+
+            return Object.assign(
+                {},
+                this.defaultConfig,
+                {
+                    enableTime: this.enableTime,
+                    noCalendar: this.noCalendar
+                },
+                newConfig,
+                {
+                    mode: 'multiple'
+                }
+            );
+        }
+    }
+});
